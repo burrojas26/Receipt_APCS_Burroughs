caa6ec8f036e53cd5ad092b97d4a0b0fe44169d8
Jasper Burroughs <burrojas26@7hills.org>
2023-08-30 13:04:12 -0400

    Hello Mr. Twyford

--- Receipt.java
+++ Receipt.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Receipt {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        System.out.print("Enter name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name);
     }
 }
