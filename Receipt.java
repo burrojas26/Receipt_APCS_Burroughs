39205d79bc814e083328a065673cfd19ba39bf56
Jasper Burroughs <burrojas26@7hills.org>
2023-09-01 12:16:42 -0400

    Made a menu

--- Receipt.java
+++ Receipt.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Receipt {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter name: ");
-        String name = scan.nextLine();
-        System.out.print("Enter the city you are from: ");
-        String city = scan.nextLine();
-        System.out.println("Hello, " + name + ". You are from " + city);
+        System.out.print("Welcome to Jasper's cookie shop. \n We sell... \n Chocolate Chip \n Gingerbread \n Sugar \n Biscotti \n Peanut Butter");
+        System.out.print("Enter item: ");
+        String item = scan.nextLine();
+        System.out.print("Enter the number of " + item + "s: ");
+        int number = scan.nextInt();
+
+
+        
     }
 }
