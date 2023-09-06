e16c02d9fe5ff81f163330edca1971dd6bdec87a
Jasper Burroughs <burrojas26@7hills.org>
2023-09-06 13:07:09 -0400

    finished the program

--- Receipt.java
+++ Receipt.java
@@ -1,20 +1,14 @@
 import java.util.Scanner;
-
 public class Receipt {
     public static void main(String[] args) {
-        
-        
         String item = "";
         String receipt = "";
         String currChars = "";
-        int number;
         int itemCount = 0;
         int chars = 0;
         double price = 0.00;
-        String receiptStatement = "";
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to Jasper's cookie shop. \n We sell... \n Chocolate Chip - $1.99 \n Gingerbread - $1.99 \n Sugar - $1.99 \n Biscotti - $1.99 \n Peanut Butter - $1.99");
-        
         while (true) {
             System.out.print("Enter item: ");
             item = scan.nextLine();
@@ -22,9 +16,6 @@ public class Receipt {
                 break;
             }
             itemCount++;
-            // System.out.print("Enter the number of " + item + "s: ");
-            // number = scan.nextInt();
-            
             currChars += "* Item: ";
             for (int i = 0; i < item.length(); i++) {
                 if (chars <= 31) {
@@ -37,23 +28,24 @@ public class Receipt {
                     currChars += item.charAt(i);
                     chars = 1;
                 }
-                
             }
             receipt += currChars + "- $1.99\n";
             currChars = "";
             chars = 0;
         }
-
         receipt += currChars + "\n";
+        for (int i = 0; i < 40; i++) {
+            System.out.print("*"); 
+        }
+        System.out.println("");
+        price = itemCount*1.99;
+        receipt += "* Total: $" + price + "\n\n";
+        System.out.print(receipt);
         for (int i = 0; i < 40; i++) {
             System.out.print("*");
             
         }
-        price = itemCount*1.99;
         System.out.println("");
-        System.out.print(receipt + "* Total: $" + price + "\n\n");
+        scan.close();
     }
-     
 }
-
-//.charAt();
