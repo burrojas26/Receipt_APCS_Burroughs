b70f00fb48f9aa3f5526e0d5c90ee529a50f4b2f
Jasper Burroughs <burrojas26@7hills.org>
2023-09-06 13:02:12 -0400

    finsihed touching up details

--- Receipt.java
+++ Receipt.java
@@ -8,25 +8,26 @@ public class Receipt {
         String receipt = "";
         String currChars = "";
         int number;
+        int itemCount = 0;
         int chars = 0;
+        double price = 0.00;
         String receiptStatement = "";
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Jasper's cookie shop. \n We sell... \n Chocolate Chip \n Gingerbread \n Sugar \n Biscotti \n Peanut Butter");
+        System.out.println("Welcome to Jasper's cookie shop. \n We sell... \n Chocolate Chip - $1.99 \n Gingerbread - $1.99 \n Sugar - $1.99 \n Biscotti - $1.99 \n Peanut Butter - $1.99");
         
         while (true) {
             System.out.print("Enter item: ");
             item = scan.nextLine();
-            System.out.println(item);
             if (item == "") {
-                System.out.println("break");
                 break;
             }
+            itemCount++;
             // System.out.print("Enter the number of " + item + "s: ");
             // number = scan.nextInt();
             
-            currChars += "Item: ";
+            currChars += "* Item: ";
             for (int i = 0; i < item.length(); i++) {
-                if (chars <= 33) {
+                if (chars <= 31) {
                     currChars = currChars + (item.charAt(i));
                     chars++;
                 }
@@ -38,7 +39,7 @@ public class Receipt {
                 }
                 
             }
-            receipt += currChars + "\n";
+            receipt += currChars + "- $1.99\n";
             currChars = "";
             chars = 0;
         }
@@ -48,8 +49,9 @@ public class Receipt {
             System.out.print("*");
             
         }
+        price = itemCount*1.99;
         System.out.println("");
-        System.out.print(receipt);
+        System.out.print(receipt + "* Total: $" + price + "\n\n");
     }
      
 }
